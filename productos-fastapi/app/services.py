b6b5556054828a9a3a260557169da4816b9adaf5
Gerardo Luna Leglisse <gerwiseong@hotmail.com>
2025-07-02 10:46:43 -0600

    Arreglar rutas

--- productos-fastapi/app/services.py
+++ productos-fastapi/app/services.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
-from .models import Producto
-from .schemas import ProductoCrear
+from app.models import Producto
+from app.schemas import ProductoCrear
 
 productos: List[Producto] = []
 contador_id = 1
